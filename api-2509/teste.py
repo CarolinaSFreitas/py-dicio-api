c0e7d615391c52f302e3c271a202e1ed69534570
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-09-25 09:56:44 -0300

    Add files via upload

--- api-2509/teste.py
+++ api-2509/teste.py
@@ -1,10 +1,13 @@
+### LISTAR AS MARCAS
+
 url_api = "http://localhost:3000/"
 
 import requests
 
 response = requests.get(url_api+"marcas")
+marcas = response.json()
+#print(marcas)
 
-marcas = response.json
-
-print(marcas)
-
+## PERCORRER AS MARCAS E RETORNAR EM FORMATO DE DICIONÁRIO 
+for marca in marcas:
+    print(f"{marca['nome']} - {marca['cidade']}")
