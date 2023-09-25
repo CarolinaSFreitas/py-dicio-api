8099fcea885d4e3bfa784894331e308ff193555a
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-09-25 10:53:49 -0300

    Delete exercicios-2509/exercicios-2509 directory

--- exercicios-2509/exercicios-2509/1-ex.py
+++
@@ -1,6 +0,0 @@
-# 1) INCLUIR VINHOS - não esquecer que: precisa primeiro registrar a marca antes de registrar o vinho
-url_api = "http://localhost:3000/vinhos"
-import requests
-vinhos = {"tipo": "Suave", "marca_id": 2, "preco": 16.49, "teor": 9.0}
-response = requests.post(url_api, json=vinhos)
-print(response.json())
--- exercicios-2509/exercicios-2509/4-ex.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url_vinhos = "http://localhost:3000/vinhos"
-response= requests.get(url_vinhos)
-vinhos = response.json()
-print(response.json())
