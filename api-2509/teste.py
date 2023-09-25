40ba09f5efd46620f5232e8827f42a686858c29c
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-09-25 10:07:01 -0300

    Add files via upload

--- api-2509/teste.py
+++ api-2509/teste.py
@@ -1,4 +1,9 @@
-### LISTAR AS MARCAS
+##1) INCLUIR VINHOS
+
+
+
+# -------------------------------------------------------------------------------------------------------------------
+### 2) LISTAR AS MARCAS DE VINHO 
 
 url_api = "http://localhost:3000/"
 
@@ -8,6 +13,8 @@ response = requests.get(url_api+"marcas")
 marcas = response.json()
 #print(marcas)
 
-## PERCORRER AS MARCAS E RETORNAR EM FORMATO DE DICIONÁRIO 
+# -------------------------------------------------------------------------------------------------------------------
+## 2) PERCORRE AS MARCAS E RETORNA EM FORMATO DE DICIONÁRIO, TAMBÉM CONTA QUANTOS VINHOS TEM DAS MARCAS REGISTRADAS 
 for marca in marcas:
     print(f"{marca['nome']} - {marca['cidade']} - {len(marca['Vinhos'])} vinhos") #mostra QUANTOS vinhos tem da marca
+
